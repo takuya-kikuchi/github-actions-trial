67aa2d2068b9105e7c59736a52c5f0c8f3fb5e90
takuya kikuchi <t.kikuchi.pch@gmail.com>
2019-12-09 15:22:17 +0900

    code cleanup

--- index.js
+++ index.js
@@ -6,41 +6,34 @@ const Octokit = require("@octokit/rest");
     try {
         await main();
     } catch (e) {
-        // Deal with the fact the chain failed
+        core.setFailed(error.message);
     }
 })();
 
 async function main() {  
-    try {
-        const sourceBranchName = core.getInput('SOURCE_BRANCH_NAME');
-        const owner = core.getInput('REPOSITORY_OWNER_NAME')
-        const repo = core.getInput('REPOSITORY_NAME')
-        const apiKey = core.getInput('GIT_API_KEY')
-        const mergedPRNumber = core.getInput('MERGED_PR_NUMBER')
+  const sourceBranchName = core.getInput('SOURCE_BRANCH_NAME');
+  const owner = core.getInput('REPOSITORY_OWNER_NAME')
+  const repo = core.getInput('REPOSITORY_NAME')
+  const apiKey = core.getInput('GITHUB_TOKEN')
+  const mergedPRNumber = core.getInput('MERGED_PR_NUMBER')
 
-        const octokit = new Octokit({ auth: apiKey });
-        const pulls = await octokit.pulls.list(
-            { 
-              owner: owner,
-              repo: repo,
-              head: sourceBranchName
-            });
-        const pr = pulls.data.shift();
-        const currentBody = pr.body;
+  const octokit = new Octokit({ auth: apiKey });
+  const pulls = await octokit.pulls.list({ 
+        owner: owner,
+        repo: repo,
+        head: sourceBranchName });
+  const pr = pulls.data.shift();
+  const currentBody = pr.body;
 
-        await octokit.pulls.update({
-            owner: owner,
-            repo: repo,
-            pull_number: pr.number,
-            body: currentBody + '\n' `Merged: #${mergedPRNumber}`,
-        });
+  await octokit.pulls.update({
+      owner: owner,
+      repo: repo,
+      pull_number: pr.number,
+      body: currentBody + '\n' `Merged: #${mergedPRNumber}`,
+  });
 
-        console.log("done!");
-        
-      } catch (error) {
-        core.setFailed(error.message);
-      }
-      
-    }
+  console.log("done!");
+  
+}
 
 
